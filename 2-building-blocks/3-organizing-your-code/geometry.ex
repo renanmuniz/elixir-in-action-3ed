0bf51782d175022608e1a1475f92a7dff7638876
renanmuniz <renan.mmerlo@gmail.com>
2024-10-25 23:30:01 -0300

    Ate 3.2.3 Multiclause lambdas

--- 2-building-blocks/3-organizing-your-code/geometry.ex
+++ 2-building-blocks/3-organizing-your-code/geometry.ex
@@ -1,9 +1,27 @@
 defmodule Geometry do
-  def rectangle_area(a,b) do
-      a * b
+  def rectangle_area(a, b) do
+    a * b
   end
 
   def square_area(l) do
     l * l
+  end
+
+  # multi-clause funtions.
+  # "sobrecarga de metodos"
+  def area({:rectangle, a, b}) do
+    a * b
+  end
+
+  def area({:square, a}) do
+    a * a
+  end
+
+  def area({:circle, r}) do
+    r * r * 3.14
+  end
+
+  def area(unknown) do
+    {:error, {:unknown_shape, unknown}}
   end
 end
