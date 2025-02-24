fde71fbb9a132b0ff212973dd4501c93df7ed402
Renan Muniz <renan.mmerlo@gmail.com>
2025-02-24 09:07:01 -0300

    ate 2.3.6 Module attributes constantes e documentacao

--- 2-building-blocks/2-3-organizing-your-code/geometry_all_forms.ex
+++ 2-building-blocks/2-3-organizing-your-code/geometry_all_forms.ex
@@ -1,19 +1,31 @@
 defmodule GeometryAllForms do
+  @moduledoc "Módulo com submodulos para calculo de area de formas geometricas"
+
   defmodule Rectangle do
+    @moduledoc "Modulo com funcoes para a forma geometrica Rectangle"
+
+    @doc "Calcula a area do retangulo"
     def area(a, b) do
       a * b
     end
   end
 
   defmodule Square do
+    @moduledoc "Modulo com funcoes para a forma geometrica Square"
+
+    @doc "Calcula a area do quadrado"
     def area(a, b) do
       a * b
     end
   end
 
   defmodule Circle do
+    @moduledoc "Modulo com funcoes para a forma geometrica Circle"
+    @pi 3.14159
+
+    @doc "Calcula a area do circulo"
     def area(r) do
-      3.141516 * (r * r)
+      @pi * (r * r)
     end
   end
 end
